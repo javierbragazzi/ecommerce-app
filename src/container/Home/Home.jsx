5c54c6860fca05d35b9c091480797a6b4466a9e4
Javier Bragazzi <jbragazzi@santandertecnologia.com.ar>
2020-08-18 20:27:07 -0300

    Se agrega el componente ItemCount

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import NavBar from '../../components/NavBar/NavBar';
 import Saludo from '../../components/Saludo/Saludo';
+import CardDeckCustom from '../../components/CardDeckCustom/CardDeckCustom';
 
 function Home() {
     return (
         <>
             <NavBar/>
-            <Saludo title='¡Hola!' greetings='Que bueno tenerte aquí de nuevo :)' isShown='true' />
+            <CardDeckCustom/>
         </>
     );
   }
